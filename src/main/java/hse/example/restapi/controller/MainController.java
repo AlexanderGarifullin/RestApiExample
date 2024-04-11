883091d638956daa71ad229018834b5b4f4908e1
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 09:15:47 +0500

    add: make getAll Get request

--- src/main/java/hse/example/restapi/controller/MainController.java
+++ src/main/java/hse/example/restapi/controller/MainController.java
@@ -1,23 +1,33 @@
 package hse.example.restapi.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hse.example.restapi.entity.Cat;
 import hse.example.restapi.repository.CatRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class MainController {
 
-    @Autowired
-    private CatRepository catRepository;
+    private final CatRepository catRepository;
+    private final ObjectMapper objectMapper;
 
     @PostMapping("/api/add")
     public void addCat(@RequestBody Cat cat){
         log.info("New row: " + catRepository.save(cat));
+    }
+
+    @SneakyThrows
+    @GetMapping("api/all")
+    public String getAll(){
+        List<Cat> cats = catRepository.findAll();
+        return objectMapper.writeValueAsString(cats);
     }
 
 }
