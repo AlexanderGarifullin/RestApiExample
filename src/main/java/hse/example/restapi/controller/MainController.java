7e75e99e758268b6c2133b4193f688786bfe2a86
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 09:11:25 +0500

    add: make addCat Post request

--- src/main/java/hse/example/restapi/controller/MainController.java
+++ src/main/java/hse/example/restapi/controller/MainController.java
@@ -3,44 +3,21 @@ package hse.example.restapi.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hse.example.restapi.entity.Cat;
+import hse.example.restapi.repository.CatRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class MainController {
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private CatRepository catRepository;
 
-    @GetMapping("api/main")
-    public String mainListener() {
-        return "Hello World";
+    @PostMapping("/api/add")
+    public void addCat(@RequestBody Cat cat){
+        log.info("New row: " + catRepository.save(cat));
     }
 
-    @GetMapping("api/cat")
-    public String giveCat(){
-        Cat cat = new Cat("Malish", 12, 5);
-        String jsonData = null;
-        try {
-            jsonData = objectMapper.writeValueAsString(cat);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return jsonData;
-    }
-
-    @PostMapping("api/special")
-    public String giveSpecialCat(@RequestParam String name) {
-        Cat cat = new Cat(name, 12, 5);
-        String jsonData = null;
-        try {
-            jsonData = objectMapper.writeValueAsString(cat);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return jsonData;
-    }
 }
