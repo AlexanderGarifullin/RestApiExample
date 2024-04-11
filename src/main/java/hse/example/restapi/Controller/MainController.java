ff310d6d6ef7d26ec50e3f8cd1f9c0a0fe56cf29
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2024-04-11 08:20:05 +0500

    add: add Cat entity

--- src/main/java/hse/example/restapi/Controller/MainController.java
+++
@@ -1,8 +0,0 @@
-package hse.example.restapi.Controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class MainController {
-
-}
---
+++ src/main/java/hse/example/restapi/entity/Cat.java
@@ -0,0 +1,15 @@
+package hse.example.restapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cat {
+    private String name;
+    private int age;
+    private int weight;
+
+}
